5d574aa7385be51d3a137dafee2b1ba090a45b6e
nadhiw <nwoliye@gmail.com>
2021-07-11 21:57:24 -0500

    the files and the name

---
+++ public/service-work.js
@@ -0,0 +1,12 @@
+const FILES_TO_CACHE = [
+    '/index.html',
+    '/favicon.ico',
+    '/manifest.webmanifest',
+    '/index.js',
+    '/style.css',
+    '/db.js',
+];
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
