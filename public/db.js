81f5e6be8f1169d0676b42ff1bd745ecec64a738
nadhiw <nwoliye@gmail.com>
2021-07-11 21:38:41 -0500

    chech Database & delete pending

--- public/db.js
+++ public/db.js
@@ -18,3 +18,45 @@ request.onsuccess = function (event) {
 request.onerror = function (event) {
     console.log("Woops!" + event.target.errorCode);
 };
+
+function saveRecord(record) {
+    const transaction = db.transaction(["pending"],"readwrite");
+    const store = transaction.objectStore("pending");
+   
+    store.add(record);
+}
+
+function checkDatabase() {
+    const transaction = db.transaction(["pending"], "readwritr");
+    const store = transaction.objectStore("pending");
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+          fetch("/api/transaction/bulk", {
+            method: "POST",
+            body: JSON.stringify(getAll.result),
+            headers: {
+              Accept: "application/json, text/plain, */*",
+              "Content-Type": "application/json"
+            }
+          })
+          .then(response => response.json())
+            .then(() => {
+            // delete records if successful
+            const transaction = db.transaction(["pending"], "readwrite");
+            const store = transaction.objectStore("pending");
+            store.clear();
+            });
+        }
+    };
+}
+
+function deletePending() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    store.clear();
+  }
+  
+  // listen for app coming back online
+  window.addEventListener("online", checkDatabase);
